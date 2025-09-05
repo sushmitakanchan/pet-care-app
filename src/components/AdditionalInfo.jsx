9ed52a0fbd174fff908bdc25fb7f5bf5bf1823d2
Sushmita <sushmitakanchan622@gmail.com>
2025-09-05 18:28:14 +0530

    styling update

--- src/components/AdditionalInfo.jsx
+++ src/components/AdditionalInfo.jsx
@@ -1,19 +1,21 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { useNavigate } from 'react-router-dom'
+import { PetContext } from '../context/PetContext'
 
 const AdditionalInfo = () => {
+  const {petInfo} = useContext(PetContext)
   const navigate = useNavigate()
   return (
     <div className='bg-[#1b1a1a] h-screen w-screen m-0 p-0 flex justify-center items-center'>
     <div className='relative bg-[#FF3232] h-[40rem] w-[50rem] mt-[3rem] mb-[3rem]'>
-      <h2>Additional Info</h2>
-      <p className='text-[10px] p-[10px]'>Please provide this information so we can send you fair reminders and provide valuable advice onhow to take care of dog to his maximum benefit and health. Your activity in the app will be tracked to adjust the settings to the real situation too</p>
+      <h2 className='ml-[14rem] mt-[2rem]'>Additional Info</h2>
+      <p className='text-[10px] p-[10px] mx-[3rem]'>Please provide this information so we can send you fair reminders and provide valuable advice onhow to take care of dog to his maximum benefit and health. Your activity in the app will be tracked to adjust the settings to the real situation too.</p>
       <div>
-      <ul className = 'flex flex-col gap-[20px] text-[15px] mt-[7rem]'>
-      <li onClick={() => navigate("/feed-schedule")} className='cursor-pointer'>How many times do you feed Ghost?</li>
-      <li onClick={() => navigate("/walk-schedule")} className='cursor-pointer'>How many times do you take Ghost out for a walk?</li>
-      <li onClick={() => navigate("/bath-schedule")} className='cursor-pointer'>When do you bath Ghost?</li>
-      <li onClick={() => navigate("/play-schedule")} className='cursor-pointer'>When do you play with Ghost?</li>
+      <ul className = 'flex flex-col gap-[20px] text-[15px] mt-[2rem] mx-[2rem]'>
+      <li onClick={() => navigate("/feed-schedule")} className='cursor-pointer'>How many times do you feed {petInfo.basic.name}?</li>
+      <li onClick={() => navigate("/walk-schedule")} className='cursor-pointer'>How many times do you take {petInfo.basic.name} out for a walk?</li>
+      <li onClick={() => navigate("/bath-schedule")} className='cursor-pointer'>When do you bath {petInfo.basic.name}?</li>
+      <li onClick={() => navigate("/play-schedule")} className='cursor-pointer'>When do you play with {petInfo.basic.name}?</li>
       </ul>
       </div>
       <div className='flex gap-[20px] justify-center items-center mt-[10rem] cursor-pointer'>
